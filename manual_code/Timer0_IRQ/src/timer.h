216c5073e69da83ef6cf00a134aa46387ca51a8b
Yiqing Huang <yqihuang@gmail.com>
2021-02-15 17:07:42 -0500

    Timer0 IRQ Starter Code for P2

---
+++ manual_code/Timer0_IRQ/src/timer.h
@@ -0,0 +1,11 @@
+/**
+ * @brief timer.h - Timer header file
+ * @author Y. Huang
+ * @date 2013/02/12
+ */
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+extern uint32_t timer_init ( uint8_t n_timer );  /* initialize timer n_timer */
+
+#endif /* ! _TIMER_H_ */
