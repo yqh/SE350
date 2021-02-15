8557c355b4588d8bec1e5ef824b0cd948486667c
Yiqing Huang <yqihuang@gmail.com>
2021-02-15 17:23:47 -0500

    API header files

---
+++ manual_code/INC/rtx_ext.h
@@ -0,0 +1,52 @@
+/*
+ ****************************************************************************
+ *
+ *                  UNIVERSITY OF WATERLOO SE 350 RTX LAB  
+ *
+ *                     Copyright 2020-2021 Yiqing Huang
+ *                          All rights reserved.
+ *---------------------------------------------------------------------------
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice and the following disclaimer.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *---------------------------------------------------------------------------*/
+ 
+
+/**************************************************************************//**
+ * @file        rtx_ext.h 
+ * @brief       RTX extended user API
+ *              
+ * @version     V1.2021.01
+ * @authors     Yiqing Huang
+ * @date        2021 JAN
+ * 
+ * @note        self-defined new RTX user API are declared in this file
+ * @see         rtx_ext.h
+ * @see         common.h
+ *****************************************************************************/
+ 
+ /*
+ *===========================================================================
+ *                            FUNCTION PROTOTYPES
+ *===========================================================================
+ */
+ 
+ /*
+ *===========================================================================
+ *                             END OF FILE
+ *===========================================================================
+ */
